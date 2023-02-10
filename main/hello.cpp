9ace588a71db60080cd33be2e9a41815029bb016
Jatinsharma159 <57961863+Jatinsharma159@users.noreply.github.com>
2023-02-10 18:39:10 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,7 +7,5 @@ int main(void) {
   cout << "Hello, Jenkins\n";
 
   cout << "I have successfully built and run\n";
-  cout<< "Webhook successfully enabled\n";
-  cout<< "Webhook verified\n";
   return 0;
 }
